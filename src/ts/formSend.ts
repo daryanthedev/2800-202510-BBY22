2d5907b4c7e396e2fd5f951bdf7b8ffea87fce78
Nerentra <60801196+Nerentra@users.noreply.github.com>
2025-04-30 14:47:57 -0700

    Add redirection after successful registration/login/logout

--- src/ts/formSend.ts
+++ src/ts/formSend.ts
@@ -1,24 +1,31 @@
-async function send(url: string, data: FormData) {
+async function send(url: string, data: FormData, redirectPath: string | undefined) {
     const obj: Record<string, FormDataEntryValue> = {};
     data.forEach((value, key) => {
         obj[key] = value;
     });
 
-    await fetch(url, {
+    const response = await fetch(url, {
         headers: {
             "Content-type": "application/json",
         },
         method: "POST",
         body: JSON.stringify(obj),
     });
+    if (response.ok) {
+        if (redirectPath !== undefined) {
+            location.assign(redirectPath);
+        } else {
+            location.reload();
+        }
+    }
 }
 
-function formSend(url: string, formElem: HTMLFormElement) {
+function formSend(url: string, formElem: HTMLFormElement, redirectPath: string | undefined) {
     formElem.addEventListener("submit", (event: SubmitEvent) => {
         event.preventDefault();
 
         const data = new FormData(formElem);
-        void send(url, data);
+        void send(url, data, redirectPath);
     });
 }
 
