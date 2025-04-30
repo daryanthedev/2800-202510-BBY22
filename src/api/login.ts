0d093df687a986c40ffe747b311db66fdb9219f8
Nerentra <60801196+Nerentra@users.noreply.github.com>
2025-04-30 14:19:17 -0700

    Make login authentication use email or password in one field

--- src/api/login.ts
+++ src/api/login.ts
@@ -4,71 +4,54 @@ import { Db } from "mongodb";
 import * as hash from "../utils/hash.js";
 import { isUsersSchema, isUsername, isEmail, isPassword, Username, Email, Password } from "../schema.js";
 
-// The login API endpoint takes either a UsernameLogin or EmailLogin
-interface UsernameLogin {
-    username: Username;
+interface LoginData {
+    usernameEmail: Username | Email;
     password: Password;
 }
 
-interface EmailLogin {
-    email: Email;
-    password: Password;
-}
-
-function isUsernameLogin(data: unknown): data is UsernameLogin {
+function isLoginData(data: unknown): data is LoginData {
     if (typeof data !== "object" || data === null) {
         return false;
     }
 
     const obj = data as Record<string, unknown>;
-    return typeof obj.username === "string" && typeof obj.password === "string"
-        && isUsername(obj.username) && isPassword(obj.password);
-}
-
-function isEmailLogin(data: unknown): data is EmailLogin {
-    if (typeof data !== "object" || data === null) {
-        return false;
-    }
-
-    const obj = data as Record<string, unknown>;
-    return typeof obj.email === "string" && typeof obj.password === "string"
-        && isEmail(obj.email) && isPassword(obj.password);
+    return typeof obj.usernameEmail === "string" && typeof obj.password === "string"
+        && (isUsername(obj.usernameEmail) || isEmail(obj.usernameEmail)) && isPassword(obj.password);
 }
 
 export default (app: Express, database: Db) => {
     app.post("/api/login", async (req: Request, res: Response) => {
-        if (isUsernameLogin(req.body)) {
-            const { username, password } = req.body;
+        if (isLoginData(req.body)) {
+            const { usernameEmail, password } = req.body;
+            if(isEmail(usernameEmail)) {
+                const user = await database
+                    .collection("users")
+                    .findOne({
+                        email: usernameEmail,
+                    });
+                if(isUsersSchema(user)) {
+                    if(await hash.compare(password, user.passwordHash)) {
+                        req.session.loggedInUserId = user._id.toHexString();
+                        res.send();
+                    }
+                    return;
+                }
+            }
             const user = await database
                 .collection("users")
                 .findOne({
-                    username,
+                    username: usernameEmail,
                 });
-            if(!isUsersSchema(user)) {
-                res.status(500).send("Internal server error.");
-                console.error(`Error: Couldn't find user with name "${username}".`);
-                return;
+            if(isUsersSchema(user)) {
+                if(await hash.compare(password, user.passwordHash)) {
+                    req.session.loggedInUserId = user._id.toHexString();
+                    res.send();
+                    return;
+                }
             }
-            if(await hash.compare(password, user.passwordHash)) {
-                req.session.loggedInUserId = user._id.toHexString();
-                res.send();
-            }
-        } else if (isEmailLogin(req.body)) {
-            const { email, password } = req.body;
-            const user = await database
-                .collection("users")
-                .findOne({
-                    email,
-                });
-            if(!isUsersSchema(user)) {
-                res.status(500).send("Internal server error.");
-                console.error(`Error: Couldn't find user with email "${email}".`);
-                return;
-            }
-            if(await hash.compare(password, user.passwordHash)) {
-                req.session.loggedInUserId = user._id.toHexString();
-                res.send();
-            }
+            res.status(500).send("Internal server error.");
+            console.error(`Error: Couldn't find user with name/email "${usernameEmail}".`);
+            return;
         } else {
             res.status(400).send("Invalid data.");
             return;
