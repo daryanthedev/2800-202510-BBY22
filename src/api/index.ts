64fc494cd84940fddd1646eb57e1463fa7aeb47f
Nerentra <60801196+Nerentra@users.noreply.github.com>
2025-05-14 13:15:43 -0700

    Separate streak apis into multiple files

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,6 +6,7 @@ export default async (app: Express, database: Db) => {
     (await import("./login.js")).default(app, database);
     (await import("./logout.js")).default(app);
     (await import("./weather.js")).default(app);
-    (await import("./streak.js")).default(app, database);
+    (await import("./streak/info.js")).default(app, database);
+    (await import("./streak/continue.js")).default(app, database);
     (await import("./account/setUsername.js")).default(app, database);
 };
