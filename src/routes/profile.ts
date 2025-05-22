8aa1699feb3994a8c907443d21a98b8d11c9707d
ahjgithub02 <hwanga01@gmail.com>
2025-05-22 14:23:14 -0700

    ran npm run format

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -10,7 +10,6 @@ export default (app: Express, database: Db) => {
     // 1) VIEW PROFILE
     //
     app.get("/profile", async (req: Request, res: Response) => {
-
         // a) Authentication guard
         if (!req.session.loggedInUserId) {
             throw new StatusError(401, "Please log in first");
